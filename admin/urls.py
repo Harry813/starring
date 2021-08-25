bbc89974790931357198c04b8b018f55d66986b3
Harry813 <wangaoharry@hotmail.com>
2021-08-26 00:32:28 +0100

    urls.py: + modify admin urls
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/urls.py
+++ admin/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='ADMLogin'),
+    path('template/', views.admin_article_create_view, name='ADMArticleCreate')
 ]
