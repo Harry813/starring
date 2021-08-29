01697ee61f7dc2e310b834e61857ae70821f0a2e
Harry813 <wangaoharry@hotmail.com>
2021-08-29 05:50:41 +0100

    settings.py: + add django-hreflang middle-ware
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- staring/settings.py
+++ staring/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'hreflang.AddHreflangToResponse'
 ]
 
 ROOT_URLCONF = 'staring.urls'
