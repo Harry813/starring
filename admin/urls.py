1b15076d631f7aa674061920157b2ae6adba0363
Harry813 <wangaoharry@hotmail.com>
2021-08-29 03:39:32 +0100

    urls.py: + add index view
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/urls.py
+++ admin/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='ADMIndex'),
+    path('', views.admin_index_view, name='ADMIndex'),
     path('login/', views.admin_login_view, name='ADMLogin'),
     path('template/', views.admin_article_create_view, name='ADMArticleCreate')
 ]
