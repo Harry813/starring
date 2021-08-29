29c49f5f3257f34c356d4cf0d1f629880214a6ff
Harry813 <wangaoharry@hotmail.com>
2021-08-29 12:36:42 +0100

    avatar: + add default avatar
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- staring/urls.py
+++ staring/urls.py
@@ -25,6 +25,7 @@ urlpatterns = i18n_patterns(
     # path('dadmin/', dadmin.site.urls),
     path('', include("customer.urls")),
     path('admin/', include("admin.urls"))
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
