4d6a40197abaff5633b31bbb57d2bd79f5bcc21e
Harry813 <wangaoharry@hotmail.com>
2021-08-30 18:51:09 +0100

    urls.py: + add page parameter to article index
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/urls.py
+++ admin/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.admin_index_view, name='ADMIndex'),
     path('login/', views.admin_login_view, name='ADMLogin'),
+    path('articles/page<int:page>', views.admin_article_index_view, name="ADMArticleIndex"),
     path('template/', views.admin_article_create_view, name='ADMArticleCreate')
 ]
