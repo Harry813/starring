c9b0cf0238f25127b6491ab3eacd64ee4137dde2
Harry813 <wangaoharry@hotmail.com>
2021-09-12 20:57:19 +0100

    forms.py: + change ContactForm structure
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/forms.py
+++ customer/forms.py
@@ -4,33 +4,17 @@ from django.utils.translation import pgettext as _p
 from django.utils.translation import ngettext as _n
 
 from staring.text import *
+from staring.models import *
 
 
-class ContactForm(forms.Form):
-    name = forms.CharField(
-        min_length=2,
-        label=ContactForm_name_text,
-        widget=forms.TextInput(attrs={"placeholder": ContactForm_name_placeholder}),
-        error_messages={
-            "required": ContactForm_name_err_required
-        }
-    )
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Consult
+        fields = ["name", "email", "contact_detail", "query"]
 
-    email = forms.EmailField(
-        label=ContactForm_email_text,
-        widget=forms.EmailInput(attrs={"placeholder": ContactForm_email_placeholder})
-    )
-
-    contact_detail = forms.CharField(
-        label=ContactForm_contact_text,
-        widget=forms.TextInput(
-            attrs={"placeholder": ContactForm_contact_placeholder}
-        )
-    )
-
-    query = forms.CharField(
-        label=ContactForm_query_text,
-        widget=forms.Textarea(
-            attrs={"placeholder": ContactForm_query_placeholder}
-        )
-    )
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update({"placeholder": consult_name_placeholder})
+        self.fields["email"].widget.attrs.update({"placeholder": consult_email_placeholder})
+        self.fields["contact_detail"].widget.attrs.update({"placeholder": consult_contact_placeholder})
+        self.fields["query"].widget.attrs.update({"placeholder": consult_query_placeholder})
