508b58b8deb46b7b8c1788bbb495c32a6d99376c
Harry813 <wangaoharry@hotmail.com>
2021-10-10 14:37:16 +0100

    add customer login form
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/forms.py
+++ customer/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
 from django.utils.translation import pgettext as _p
 from django.utils.translation import ngettext as _n
 
+from staring.models import User
 from .models import Consult
 from staring.text import *
 
@@ -18,3 +21,41 @@ class ContactForm(forms.ModelForm):
         self.fields["email"].widget.attrs.update({"placeholder": consult_email_placeholder})
         self.fields["contact_detail"].widget.attrs.update({"placeholder": consult_contact_placeholder})
         self.fields["query"].widget.attrs.update({"placeholder": consult_query_placeholder})
+
+
+class CustomerLoginForm(forms.Form):
+    username_validator = UnicodeUsernameValidator()
+    username = forms.CharField(
+        label=user_username_text,
+        max_length=150,
+        min_length=8,
+        validators=[username_validator],
+        help_text=user_username_help_text,
+        error_messages={
+            "invalid": user_username_err_invalid,
+            "max_length": user_username_err_max,
+            "min_length": user_username_err_min,
+            "UserNotExist": UserNotExist_text,
+        },
+    )
+
+    password = forms.CharField(
+        label=user_password_text,
+        max_length=128,
+        min_length=8,
+        widget=forms.PasswordInput,
+        help_text=user_password_help_text,
+        error_messages={
+            "invalid": user_password_err_invalid,
+            "max_length": user_password_err_max_length,
+            "min_length": user_password_err_min_length
+        }
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username):
+            return username
+        else:
+            raise ValidationError(UserNotExist_text, code="UserNotExist")
+
