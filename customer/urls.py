89beb42c53b237a5327ad83daf6c62810141fdc9
Harry813 <wangaoharry@hotmail.com>
2021-10-17 03:25:24 +0100

    add customer contact iframe and url
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/urls.py
+++ customer/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('article/<int:article_id>/', views.customer_articles, name="article")
+    path('', views.index, name='CUSTIndex'),
+    path('article/<int:article_id>/', views.customer_articles, name="article"),
+    path('login/', views.customer_login, name="CUSTLogin"),
+    path('logout/', views.customer_logout, name="CUSTLogout"),
+    path('register/', views.customer_register, name="CUSTRegister"),
+    path('profile/', views.customer_center_view, name="CUSTCenter"),
+    path('search/<int:page>/', views.customer_search_view, name="CUSTSearch"),
+    path('contact_form/', views.customer_contact_form_frame, name="CUSTContact")
 ]
