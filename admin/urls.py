f6c42b163320656bfad5985809f5c17ab94501ae
Harry813 <wangaoharry@hotmail.com>
2021-10-18 02:08:49 +0100

    login fixed
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/urls.py
+++ admin/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.admin_index_view, name='ADMIndex'),
-    path('login/', views.admin_login_view, name='ADMLogin'),
+    re_path(r'login/$', views.admin_login_view, name='ADMLogin'),
     path('logout/', views.admin_logout_view, name='ADMLogout'),
     path('articles/page<int:page>/', views.admin_article_index_view, name="ADMArticleIndex"),
     path('articles/create/', views.admin_article_create_view, name="ADMArticleCreate"),
