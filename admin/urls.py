e7e1e09e20210c06a110f7a1f5b041243be9ef34
Harry813 <wangaoharry@hotmail.com>
2021-10-21 00:28:38 +0100

    customer edit function reconstruct
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/urls.py
+++ admin/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('articles/create/', views.admin_article_create_view, name="ADMArticleCreate"),
     path('articles/article-<int:article_id>/', views.admin_article_edit_view, name="ADMArticleEdit"),
     path('customer/page<int:page>/', views.admin_customer_index_view, name="ADMCustomerIndex"),
-    path('customer/profile-<str:customer_id>/', views.admin_customer_edit_view, name="ADMCustomerEdit")
+    path('customer/profile-<str:customer_id>/', views.admin_customer_edit_view, name="ADMCustomerEdit"),
+    path('customer/profile-<str:customer_id>/basic/', views.admin_customer_basic_edit_view,
+         name="ADMCustomerBasicEdit"),
+    path('customer/profile-<str:customer_id>/profile/', views.admin_customer_profile_edit_view,
+         name="ADMCustomerProfileEdit")
 ]
