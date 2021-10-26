8724c21a6e4c870d71bdb7d2fff13190f60737b9
Harry813 <wangaoharry@hotmail.com>
2021-10-26 18:12:54 +0100

    bug fix
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- admin/views.py
+++ admin/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.translation import gettext as _
