352c610b6c116e3cfd641ded583eb49999fed94b
Harry813 <wangaoharry@hotmail.com>
2021-12-27 11:48:17 -0800

    Update:   - Update get_customer_info in utils
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/utils.py
+++ customer/utils.py
@@ -1,13 +1,19 @@
-from staring.customerSettings import IndexListItems, NavItems
-from staring.models import NewsSector, News, NavigatorSector
+from staring.customerSettings import IndexListItems, navi_item_per_col
+from staring.models import NewsSector, News, NavigatorSector, NavigatorItem
 
 
 def get_customer_info():
     dic = {
         "indexList": IndexListItems,
-        "navItems": NavItems,
-        "navigatorSectors": NavigatorSector.objects.all(),
+        "navigator": []
     }
+
+    for sec in NavigatorSector.objects.all():
+        itemslist = list(NavigatorItem.objects.filter(sector=sec))
+        dic["navigator"].append({
+            "sector": sec,
+            "itemslist": [itemslist[i: i+navi_item_per_col] for i in range(0, len(itemslist), navi_item_per_col)]
+        })
     return dic
 
 
