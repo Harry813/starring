a3dee7d0c16401df1d27962c8d85ceb07760c0a0
Harry813 <wangaoharry@hotmail.com>
2022-06-12 00:04:08 +0100

    Update:   - Add Customer Appointment url entrance on step 1 & 2
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/urls.py
+++ customer/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('logout/', views.customer_logout, name="CUSTLogout"),
     path('register/', views.customer_register, name="CUSTRegister"),
     path('profile/', views.customer_center_view, name="CUSTCenter"),
+    path('appointment/', views.customer_appointment_view, name="CUSTAppoint"),
+    path('appointment/<str:slot_id>/', views.customer_appointment_2_view, name="CUSTAppoint2"),
     path('search/<int:page>/', views.customer_search_view, name="CUSTSearch"),
     path('contact_form/', views.customer_contact_form_frame, name="CUSTContact")
 ]
