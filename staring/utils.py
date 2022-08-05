f9004cff922f3ec5ea79dfeffc7e73570d633c8b
Harry813 <wangaoharry@hotmail.com>
2022-08-05 13:04:55 +0100

    Update:   - Formalize
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- staring/utils.py
+++ staring/utils.py
@@ -7,21 +7,21 @@ from .customerSettings import Languages
 from .text import *
 
 
-def get_language_codes():
+def get_language_codes ():
     langs = []
     for lang in Languages:
         langs.append((lang["CODE"], lang["NAME"]))
     return tuple(langs)
 
 
-def get_basic_info():
+def get_basic_info ():
     param = {
         "brandname": brand_name
     }
     return param
 
 
-def get_upload_directory():
+def get_upload_directory ():
     date_path = datetime.now().strftime("%Y/%m/%d")
 
     # Complete upload path (upload_path + date_path).
@@ -29,22 +29,22 @@ def get_upload_directory():
     return os.path.join(settings.MEDIA_ROOT, upload_path)
 
 
-def get_media_url(file_name):
+def get_media_url (file_name):
     args = [settings.CKEDITOR_UPLOAD_PATH, datetime.now().strftime("%Y/%m/%d"), file_name]
     return settings.MEDIA_URL + "/".join(arg.strip("/") for arg in args)
 
 
-def remove_upload_directory():
+def remove_upload_directory ():
     # Called on test setup
     # Avoid falling in the use case chere django append a hash to the file name
     # to prevent file collisions
     shutil.rmtree(get_upload_directory(), ignore_errors=True)
 
 
-def get_absolute_media_path(file_name):
+def get_absolute_media_path (file_name):
     upload_directory = get_upload_directory()
     return os.path.join(upload_directory, file_name)
 
 
-def get_absolute_name(class_or_function):
+def get_absolute_name (class_or_function):
     return "%s.%s" % (class_or_function.__module__, class_or_function.__name__)
