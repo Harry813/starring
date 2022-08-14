1020b6879ac6ce4c2460e25c7ab929c9f0981829
Harry813 <wangaoharry@hotmail.com>
2022-08-14 17:16:58 +0100

    update:   - change index rendering
    Signed-off-by: Harry813 <wangaoharry@hotmail.com>

--- customer/views.py
+++ customer/views.py
@@ -41,11 +41,11 @@ def index (request):
         "indexList": get_index_list(),
         **get_customer_info()
     }
-    if request.user_agent.is_mobile:
-        # return render(request, "customer/tele/tele_index1.html", param)
-        return render(request, "customer/tele/tele_index.html", param)
-    else:
-        return render(request, "customer/index.html", param)
+    # if request.user_agent.is_mobile:
+    #     # return render(request, "customer/tele/tele_index1.html", param)
+    #     return render(request, "customer/tele/tele_index.html", param)
+    # else:
+    return render(request, "customer/index.html", param)
 
 
 def customer_login_view (request):
